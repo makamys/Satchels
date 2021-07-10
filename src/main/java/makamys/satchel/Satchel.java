f0245398fbd468dcab74b46379a4eccacea030c7
makamys <makamys@outlook.com>
2021-07-10 14:22:45 +0200

    Init project

--- src/main/java/makamys/satchel/Satchel.java
+++ src/main/java/makamys/satchel/Satchel.java
@@ -1,15 +1,15 @@
-package ${package};
+package makamys.satchel;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Satchel.MODID, version = Satchel.VERSION)
+public class Satchel
 {
-    public static final String MODID = "${modid}";
-    public static final String VERSION = "${version}";
+    public static final String MODID = "satchel";
+    public static final String VERSION = "0.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
