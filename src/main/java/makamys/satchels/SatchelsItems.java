9e341778be04e7c2e565a88ba12828e2b7b080c7
makamys <makamys@outlook.com>
2021-07-18 03:02:36 +0200

    Add pouch item

--- src/main/java/makamys/satchels/SatchelsItems.java
+++ src/main/java/makamys/satchels/SatchelsItems.java
@@ -1,6 +1,7 @@
 package makamys.satchels;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import makamys.satchels.item.ItemPouch;
 import makamys.satchels.item.ItemSatchel;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -9,6 +10,7 @@ import net.minecraft.item.ItemStack;
 public class SatchelsItems {
 	
 	public static final Item satchel = initItem(new ItemSatchel());
+	public static final Item pouch = initItem(new ItemPouch());
 	
 	public static void init() {
     	registerRecipes();
@@ -16,6 +18,7 @@ public class SatchelsItems {
 	
 	private static void registerRecipes() {
 		GameRegistry.addShapedRecipe(new ItemStack(satchel), new Object[] {"L L", "LDL", "LLL", 'L', Items.leather, 'D', Items.diamond});
+		GameRegistry.addShapedRecipe(new ItemStack(pouch), new Object[] {" L ", "LSL", " L ", 'L', Items.leather, 'S', Items.string});
 	}
 	
 	private static Item initItem(Item item) {
--- src/main/java/makamys/satchels/item/ItemPouch.java
+++ src/main/java/makamys/satchels/item/ItemPouch.java
@@ -1,7 +1,24 @@
 package makamys.satchels.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import makamys.satchels.Satchels;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ItemPouch extends Item {
-
+	
+	public ItemPouch() {
+		setMaxStackSize(1);
+		setUnlocalizedName(Satchels.MODID + "." + "pouch");
+		setCreativeTab(CreativeTabs.tabTools);
+		setTextureName("pouch");
+	}
+	
+   @SideOnly(Side.CLIENT)
+   public void registerIcons(IIconRegister iconRegister) {
+	   super.itemIcon = iconRegister.registerIcon("satchels:pouch");
+   }
+   
 }
