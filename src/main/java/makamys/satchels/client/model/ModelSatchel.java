17a2194d30f2296951cb85f490d9cf822ef431e4
makamys <makamys@outlook.com>
2021-07-21 17:48:50 +0200

    Add strap to satchel model

--- src/main/java/makamys/satchels/client/model/ModelSatchel.java
+++ src/main/java/makamys/satchels/client/model/ModelSatchel.java
@@ -14,19 +14,34 @@ public class ModelSatchel extends ModelBiped {
 	
 	public ModelSatchel(){
 		bipedBody = new ModelRenderer(this);
-		
-		ModelRenderer satchel = new ModelRenderer(this);
-		float x0 = -7.5f;
-		float y0 = 13;
-		float z0 = -4;
-		int dx = 2;
-		int dy = 7;
-		int dz = 8;
-		
-		satchel.addBox(0, 0, 0, dx, dy, dz);
-		satchel.setRotationPoint(x0, y0, z0);
-		bipedBody.addChild(satchel);
-		satchel.rotateAngleZ = 0.05f;
+		{
+			ModelRenderer satchel = new ModelRenderer(this);
+			float x0 = -7.5f;
+			float y0 = 13;
+			float z0 = -4;
+			int dx = 2;
+			int dy = 7;
+			int dz = 8;
+			
+			satchel.addBox(0, 0, 0, dx, dy, dz);
+			satchel.setRotationPoint(x0, y0, z0);
+			bipedBody.addChild(satchel);
+			satchel.rotateAngleZ = 0.05f;
+		}
+		{
+			ModelRenderer strap = new ModelRenderer(this);
+			float x0 = -6f;
+			float y0 = 13;
+			float z0 = -4;
+			int dx = 1;
+			int dy = -19;
+			int dz = 8;
+			
+			strap.addBox(0, 0, 0, dx, dy, dz);
+			strap.setRotationPoint(x0-0.2f, y0+0.4f, z0);
+			bipedBody.addChild(strap);
+			strap.rotateAngleZ = 0.75f;
+		}
 	}
 	
 	@Override
