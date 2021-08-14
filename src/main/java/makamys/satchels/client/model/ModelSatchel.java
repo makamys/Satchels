d3b83a3fa5026106a38c9d015ee31edf9a27f6cc
makamys <makamys@outlook.com>
2021-08-14 17:28:50 +0200

    Refactor ModelSatchel

--- src/main/java/makamys/satchels/client/model/ModelSatchel.java
+++ src/main/java/makamys/satchels/client/model/ModelSatchel.java
@@ -15,8 +15,8 @@ public class ModelSatchel extends ModelAccessory {
 	public static ModelAccessory instance;
 	AccessoryModelRenderer satchel;
 	AccessoryModelRenderer strap;
-	AccessoryModelRenderer satchelBig;
-	AccessoryModelRenderer strapBig;
+	AccessoryModelRenderer satchelChestplate;
+	AccessoryModelRenderer strapChestplate;
 	
 	private static final ResourceLocation TEXTURE = new ResourceLocation(Satchels.MODID, "textures/models/satchel.png");
 	
@@ -25,10 +25,10 @@ public class ModelSatchel extends ModelAccessory {
 	    
 	    for(AccessoryModelRenderer amr : Arrays.asList(
 	            satchel = new AccessoryModelRenderer(rp.modelBipedMain),
-	            satchelBig = new AccessoryModelRenderer(rp.modelBipedMain)))
+	            satchelChestplate = new AccessoryModelRenderer(rp.modelArmorChestplate)))
 		{
 			amr.setTexture(TEXTURE)
-			        .setScaleMod(DEFAULT_SCALE_MOD * (amr == satchelBig ? 1.2f : 1f));
+			        .setScaleMod(DEFAULT_SCALE_MOD * (amr == satchelChestplate ? 1.2f : 1f));
 			amr.textureWidth = amr.textureHeight = 32;
 			float x0 = -7.5f;
 			float y0 = 13;
@@ -39,16 +39,16 @@ public class ModelSatchel extends ModelAccessory {
 			
 			amr.addBox(0, 0, 0, dx, dy, dz);
 			amr.setRotationPoint(x0, y0, z0);
-			rp.modelBipedMain.bipedBody.addChild(amr);
+			(amr == satchelChestplate ? rp.modelArmorChestplate : rp.modelBipedMain).bipedBody.addChild(amr);
 			amr.rotateAngleZ = 0.05f;
 			amr.rotateAngleY = (float)(Math.PI / 2f);
 		}
 	    for(AccessoryModelRenderer amr : Arrays.asList(
                 strap = new AccessoryModelRenderer(rp.modelBipedMain, 0, 9),
-                strapBig = new AccessoryModelRenderer(rp.modelBipedMain, 0, 9)))
+                strapChestplate = new AccessoryModelRenderer(rp.modelArmorChestplate, 0, 9)))
         {
 	        amr.setTexture(TEXTURE)
-	                .setScaleMod(DEFAULT_SCALE_MOD * (amr == strapBig ? 1.2f : 1f) * 1.001f);
+	                .setScaleMod(DEFAULT_SCALE_MOD * (amr == strapChestplate ? 1.2f : 1f) * 1.001f);
 	        amr.textureWidth = amr.textureHeight = 32;
 			float x0 = 7.5f;
 			float y0 = -0.1f;
@@ -59,7 +59,7 @@ public class ModelSatchel extends ModelAccessory {
 			
 			amr.addBox(0, 0, 0, dx, dy, dz);
 			amr.setRotationPoint(x0-0.2f, y0+0.4f, z0);
-			rp.modelBipedMain.bipedBody.addChild(amr);
+			(amr == strapChestplate ? rp.modelArmorChestplate : rp.modelBipedMain).bipedBody.addChild(amr);
 			amr.rotateAngleY = -(float)(Math.PI / 2f);
 			amr.rotateAngleZ = 0.75f;
 		}
@@ -70,9 +70,9 @@ public class ModelSatchel extends ModelAccessory {
         boolean hasChestplate = event.entityPlayer.getCurrentArmor(2) != null;
         
         boolean draw = EntityPropertiesSatchels.fromPlayer(event.entityPlayer).hasSatchel();
-        for(ModelRenderer amr : Arrays.asList(satchel, strap, satchelBig, strapBig)) {
-            boolean sizeOk = (!hasChestplate && (amr == satchel || amr == strap)) || (hasChestplate && (amr == satchelBig || amr == strapBig));
-            amr.isHidden = !draw || !sizeOk || (amr == satchel ? !ConfigSatchels.drawSatchel : (!ConfigSatchels.drawSatchel || !ConfigSatchels.drawSatchelStrap));
+        for(ModelRenderer amr : Arrays.asList(satchel, strap, satchelChestplate, strapChestplate)) {
+            boolean sizeOk = (!hasChestplate && (amr == satchel || amr == strap)) || (hasChestplate && (amr == satchelChestplate || amr == strapChestplate));
+            amr.showModel = draw && sizeOk && (amr == satchel ? ConfigSatchels.drawSatchel : (ConfigSatchels.drawSatchel && ConfigSatchels.drawSatchelStrap));
             amr.offsetY = hasChestplate ? -0.08f : 0f;
         }
     }
