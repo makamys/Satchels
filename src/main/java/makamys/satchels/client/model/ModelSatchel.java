f6c231800472acee74da34feb7f48da29c79c49a
makamys <makamys@outlook.com>
2021-08-14 16:54:48 +0200

    Use upscaled satchel model when wearing chestplate

--- src/main/java/makamys/satchels/client/model/ModelSatchel.java
+++ src/main/java/makamys/satchels/client/model/ModelSatchel.java
@@ -1,31 +1,37 @@
 package makamys.satchels.client.model;
 
 import java.util.Arrays;
-import java.util.List;
 
 import makamys.satchels.ConfigSatchels;
 import makamys.satchels.EntityPropertiesSatchels;
 import makamys.satchels.Satchels;
-import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 
 public class ModelSatchel extends ModelWearable {
 	
 	public static ModelWearable instance;
-	ModelRenderer satchel;
-	ModelRenderer strap;
+	AccessoryModelRenderer satchel;
+	AccessoryModelRenderer strap;
+	AccessoryModelRenderer satchelBig;
+	AccessoryModelRenderer strapBig;
 	
 	private static final ResourceLocation TEXTURE = new ResourceLocation(Satchels.MODID, "textures/models/satchel.png");
 	
 	public ModelSatchel(RenderPlayer rp){
 	    super(rp);
+	    
+	    float baseScale = 16f / 24f;
+	    
+	    for(AccessoryModelRenderer amr : Arrays.asList(
+	            satchel = new AccessoryModelRenderer(rp.modelBipedMain),
+	            satchelBig = new AccessoryModelRenderer(rp.modelBipedMain)))
 		{
-			satchel = new AccessoryModelRenderer(rp.modelBipedMain).setTexture(TEXTURE).setScaleMod(16f / 24f);
-			satchel.textureWidth = satchel.textureHeight = 32;
+			amr.setTexture(TEXTURE)
+			        .setScaleMod(baseScale * (amr == satchelBig ? 1.2f : 1f));
+			amr.textureWidth = amr.textureHeight = 32;
 			float x0 = -7.5f;
 			float y0 = 13;
 			float z0 = 4;
@@ -33,15 +39,19 @@ public class ModelSatchel extends ModelWearable {
 			int dy = 7;
 			int dz = 2;
 			
-			satchel.addBox(0, 0, 0, dx, dy, dz);
-			satchel.setRotationPoint(x0, y0, z0);
-			rp.modelBipedMain.bipedBody.addChild(satchel);
-			satchel.rotateAngleZ = 0.05f;
-			satchel.rotateAngleY = (float)(Math.PI / 2f);
+			amr.addBox(0, 0, 0, dx, dy, dz);
+			amr.setRotationPoint(x0, y0, z0);
+			rp.modelBipedMain.bipedBody.addChild(amr);
+			amr.rotateAngleZ = 0.05f;
+			amr.rotateAngleY = (float)(Math.PI / 2f);
 		}
-		{
-			strap = new AccessoryModelRenderer(rp.modelBipedMain, 0, 9).setTexture(TEXTURE).setScaleMod((16f / 24f) * 1.001f);
-			strap.textureWidth = strap.textureHeight = 32;
+	    for(AccessoryModelRenderer amr : Arrays.asList(
+                strap = new AccessoryModelRenderer(rp.modelBipedMain, 0, 9),
+                strapBig = new AccessoryModelRenderer(rp.modelBipedMain, 0, 9)))
+        {
+	        amr.setTexture(TEXTURE)
+	                .setScaleMod(baseScale * (amr == strapBig ? 1.2f : 1f) * 1.001f);
+	        amr.textureWidth = amr.textureHeight = 32;
 			float x0 = 7.5f;
 			float y0 = -0.1f;
 			float z0 = -4;
@@ -49,36 +59,23 @@ public class ModelSatchel extends ModelWearable {
 			int dy = 19;
 			int dz = 1;
 			
-			strap.addBox(0, 0, 0, dx, dy, dz);
-			strap.setRotationPoint(x0-0.2f, y0+0.4f, z0);
-			rp.modelBipedMain.bipedBody.addChild(strap);
-			strap.rotateAngleY = -(float)(Math.PI / 2f);
-			strap.rotateAngleZ = 0.75f;
+			amr.addBox(0, 0, 0, dx, dy, dz);
+			amr.setRotationPoint(x0-0.2f, y0+0.4f, z0);
+			rp.modelBipedMain.bipedBody.addChild(amr);
+			amr.rotateAngleY = -(float)(Math.PI / 2f);
+			amr.rotateAngleZ = 0.75f;
 		}
 	}
-	/*
-	@Override
-	protected float getScalePreRender(Entity entity, ModelBiped biped, boolean hasChestplate, EntityPropertiesSatchels props) {
-		
-	}
-	
-	@Override
-	protected List<ModelBiped> getBipeds() {
-		return Arrays.asList(satchelBiped, strapBiped);
-	}*/
 
 	@Override
 	public void renderPlayer(RenderPlayerEvent.Specials.Pre event) {
-        boolean hasChestplate = false;//event.entityPlayer.getCurrentArmor(2) != null;
+        boolean hasChestplate = event.entityPlayer.getCurrentArmor(2) != null;
         
         boolean draw = EntityPropertiesSatchels.fromPlayer(event.entityPlayer).hasSatchel();
-        for(ModelRenderer amr : Arrays.asList(satchel, strap)) {
-            amr.isHidden = draw && (amr == satchel ? !ConfigSatchels.drawSatchel : (!ConfigSatchels.drawSatchel || !ConfigSatchels.drawSatchelStrap));
+        for(ModelRenderer amr : Arrays.asList(satchel, strap, satchelBig, strapBig)) {
+            boolean sizeOk = (!hasChestplate && (amr == satchel || amr == strap)) || (hasChestplate && (amr == satchelBig || amr == strapBig));
+            amr.isHidden = !draw || !sizeOk || (amr == satchel ? !ConfigSatchels.drawSatchel : (!ConfigSatchels.drawSatchel || !ConfigSatchels.drawSatchelStrap));
             amr.offsetY = hasChestplate ? -0.08f : 0f;
-            
-            //float scale = hasChestplate ? 1/20f : 1/24f;
-            
-            //return scale * (biped == satchelBiped ? 1f : 1.01f);
         }
     }
 	
