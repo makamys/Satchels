0e06d6c3b4e75886bb70c821d22f2e94556d255d
makamys <makamys@outlook.com>
2022-05-07 01:59:47 +0200

    Fix changing game mode via NEI's button not being handled correctly

--- src/main/java/makamys/satchels/mixin/MixinItemInWorldManager.java
+++ src/main/java/makamys/satchels/mixin/MixinItemInWorldManager.java
@@ -6,16 +6,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import makamys.satchels.Satchels;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.management.ItemInWorldManager;
 import net.minecraft.world.WorldSettings;
 
-@Mixin(EntityPlayerMP.class)
-public abstract class MixinEntityPlayerMP {
+@Mixin(ItemInWorldManager.class)
+public abstract class MixinItemInWorldManager {
     
     @Inject(method = "setGameType", at = @At("RETURN"))
-    private void preSetGameType(WorldSettings.GameType gameType, CallbackInfo ci) {
-        Satchels.onGameTypeChanged(gameType, (EntityPlayer)(Object)this);
-    }
-    
-}
+    private void postSetGameType(WorldSettings.GameType gameType, CallbackInfo ci) {
+        Satchels.onGameTypeChanged(gameType, ((ItemInWorldManager)(Object)this).thisPlayerMP);
+    }   
+}    
